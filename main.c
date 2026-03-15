83644f517f7e6a5d5c811277eb9537d5e9aba9bc
kobkonuk <jakobkobkob@proton.me>
2026-03-15 15:38:55 +0100

    hi

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <ncurses.h>
+
+int main(void) {
+	printw("this is a work in progress, baka");
+	printw("ncurses lowk sucks but, i need it, its n-not like i want to use it or anything like that....");
+	printw("what am i, the great kobkobkobkobkob, doing with my life. talking to myself like a tsundere. ok ill go for a run");
+}
